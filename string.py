4c021ea58cea66f04136014be5dc1d4548fcfad6
fuyukoHEX <cocklessheh@gmail.com>
2026-01-25 17:06:32 +0500

    A basic algorithm for finding the number of required letters in a text

---
+++ string.py
@@ -0,0 +1,12 @@
+def count_letter(text, char):
+    count = 0
+    text = text.lower()
+    for current_char in text:
+        if current_char == char:
+            count += 1
+    return count
+
+input_text = str(input())
+target_char = input()
+
+print(count_letter(input_text, target_char))
